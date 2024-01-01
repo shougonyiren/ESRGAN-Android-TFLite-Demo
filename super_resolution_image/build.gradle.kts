184cbc6ff7505cb8d54ff1cf06b0d5fc32f988b9
shougonyiren <425769149@qq.com>
2024-01-02 01:38:02 +0800

    暂时提交

--- super_resolution_image/build.gradle.kts
+++ super_resolution_image/build.gradle.kts
@@ -40,4 +40,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //图片加载库
+    implementation("io.coil-kt:coil:2.2.2")
+
+    //工具类
+    implementation("com.blankj:utilcodex:1.31.1")
+    //kotlinx-coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 }
--- super_resolution_image/src/main/java/com/lh/SuperResolutionImage/SRImageUtils.kt
+++ super_resolution_image/src/main/java/com/lh/SuperResolutionImage/SRImageUtils.kt
@@ -1,6 +1,67 @@
+import android.content.Context
+import android.widget.ImageView
+import coil.imageLoader
+import coil.request.ImageRequest
+import com.blankj.utilcode.util.ConvertUtils
+import com.blankj.utilcode.util.LogUtils
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import java.net.URL
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+class SRImageUtils{
 
+    companion object{
+        private const val MAX_BITMAP_SIZE =
+            100 * 1024 * 1024 // 100 MB 最大的图片 trying to draw too large(159907840bytes) bitmap.
 
-class SRimageUtils{
+        private const val MODEL_NAME = "ESRGAN.tflite"
+        private const val IN_HEIGHT = 50
+        private const val IN_WIDTH = 50
+        private const val UPSCALE_FACTOR = 4
+        private val OUT_HEIGHT: Int = IN_HEIGHT * UPSCALE_FACTOR
+        private val OUT_WIDTH: Int = IN_WIDTH * UPSCALE_FACTOR
 
+        /**
+         *     todo
+         *     改造为uri
+         *     考虑 列表复用 对每个imageview进行map last处理 节流并取最后一个
+         */
+        suspend fun ImageView.loadSRImage(context: Context,url: String){
+            val imageLoader = context.imageLoader
+            val request = ImageRequest.Builder(context)
+                .data(url)//"https://img-blog.csdnimg.cn/20210124002108308.png")
+                .target(
+                    onStart = { placeholder ->
+                              LogUtils.d("onStart$url")
+                        // Handle the placeholder drawable.
+                    },
+                    onSuccess = { result ->
+                        LogUtils.d("onSuccess$url")
+                        // Handle the successful result.
+                    },
+                    onError = { error ->
+                        LogUtils.d("onError$url")
+                        // Handle the error drawable.
+                    }
+                )
+                .build()
+            flow{
+               emit(imageLoader.execute(request).drawable)
+            }.filter {
+                if (drawable != null) {
+                    true
+                } else {
+                    false
+                }
+            }.map {
+                ConvertUtils.drawable2Bitmap(it)
+            }.collect(){
+
+            }
+        }
+
+    }
 
 }
