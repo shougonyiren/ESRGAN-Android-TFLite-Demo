f8ed4c96b7cf11e84c68724d5a945d11877301c6
liuhao02 <425769149@qq.com>
2024-01-31 19:19:05 +0800

    增加适配glide库

--- super-resolution-core/src/main/java/com/lh/superresolution/core/SRImage.kt
+++ super-resolution-core/src/main/java/com/lh/superresolution/core/SRImage.kt
@@ -1,13 +1,26 @@
 package com.lh.superresolution.core
 
+import android.content.Context
+import android.graphics.Bitmap
+
 /**
 
-* @Author : liuhao02
+ * @Author : liuhao02
 
-* @Time : On 2024/1/31 14:56
+ * @Time : On 2024/1/31 15:35
 
-* @Description : SRImage
+ * @Description : SRImage
 
-*/
-class SRImage {
+ */
+abstract class SRImage(private var context: Context)  {
+    init {
+
+    }
+
+    public var isDebug: Boolean = true
+    abstract  fun loadBitmap(bitmap: Bitmap, context: Context, scale: Float): Bitmap?
+
+
+    abstract fun loadBase64ToBitmap(base64: String, context: Context, scale: Float): String
+
 }
